0d7aae4ef05d738971d08067d2ca99891aeb3c7e
SangaviVV-23 <sangavivvit@gmail.com>
2025-09-03 01:17:08 +0530

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import ExperienceSection from './components/Experience';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <ExperienceSection />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
